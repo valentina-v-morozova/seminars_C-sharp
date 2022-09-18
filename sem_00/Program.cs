279d6b65a681c90d3c7579925bfe41f4b5d6d00a
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-18 15:22:46 +0700

    base code

---
+++ sem_00/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат
+//(число умноженное на само себя).
+Console.Write("Введите число > ");
+string strValue; //объявляем тип переменной
+strValue = Console.ReadLine() ?? "0"; //если ничего не ввели - присваиваем 0
+int value = int.Parse(strValue); //переводим значение из консоли в int
+
+int result = value * value;
+Console.WriteLine($"Квадрат числа {value} равен {result}");
