09cf9c0a96295eb368cbba1da4a02b6ff16c9271
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-18 16:22:06 +0700

    задачки про дни недели 2 варианта

---
+++ sem_02_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру
+
+// Ввод данных из консоли по приглашению
+int Prompt(string message)
+{
+    System.Console.Write(message);        //Вывод приглашения
+    string strValue;                      //Объявляем переменной для ввода строки
+    strValue = Console.ReadLine() ?? "0"; //Вводим строку с консоли (можно ввести только строку)
+    int value = int.Parse(strValue);      //Преобразование строки в целое число
+    return value;
+}
+
+string[] days = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+
+int value = Prompt("Введите номер дня недели > ");
+
+if (value < 0 || value > 7)
+{
+    System.Console.WriteLine("Такого дня не существует");
+}
+else{}
+int daysValue = value -1;
+System.Console.WriteLine($"{value} -> {days[daysValue]}");
