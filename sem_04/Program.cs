f3544f5d029f11b85d1b7445574571f21ecbe429
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-18 16:38:56 +0700

    задача 4

---
+++ sem_04/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+// Ввод числа из консоли по приглашению
+int Prompt(string message)
+{
+    System.Console.Write(message);        //Вывод приглашения
+    string strValue;                      //Объявляем переменной для ввода строки
+    strValue = Console.ReadLine() ?? "0"; //Вводим строку с консоли (можно ввести только строку)
+    int value = int.Parse(strValue);      //Преобразование строки в целое число
+    return value;
+}
+
+int value = Prompt("Введите число > ");
+
+for(int index = - value; index < value+1; index++ )
+{
+    Console.Write($" {index},");
+}
+//int result = value * value* value;
+//Console.WriteLine($"Куб числа {value} равен {result}");//Вывод результата
