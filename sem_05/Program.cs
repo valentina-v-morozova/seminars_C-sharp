689b17897f4a533fcc210e76cdfbe09a801f5704
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-18 16:45:53 +0700

    вывести модуль

---
+++ sem_05/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу вычисления модуля числа.
+//	2 -> 2
+//	-3 -> 3
+//	-7 -> 7
+
+// Ввод числа из консоли по приглашению
+int Prompt(string message)
+{
+    System.Console.Write(message);        //Вывод приглашения
+    string strValue;                      //Объявляем переменной для ввода строки
+    strValue = Console.ReadLine() ?? "0"; //Вводим строку с консоли (можно ввести только строку)
+    int value = int.Parse(strValue);      //Преобразование строки в целое число
+    return value;
+}
+
+int value = Prompt("Введите число > ");
+int result;
+if (value < 0)
+{
+result = -value;
+}
+else
+ {
+result = value;
+}   
+Console.WriteLine($"{value} -> {result}");//Вывод результата
