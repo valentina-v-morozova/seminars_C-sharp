2b784201409488da4d32013178f27f708fe63172
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-24 14:13:07 +0700

    семинар 2 задача 2

---
+++ sem02_02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int randValue = new Random().Next(10, 100); // выводим случайное значение
+Console.WriteLine(randValue);
+
+int firstNumber = randValue / 10;
+int secondNumber = randValue % 10;
+int maxNumber = firstNumber;
+
+if (maxNumber < secondNumber)
+{
+    maxNumber = secondNumber;
+}
+
+    Console.WriteLine(maxNumber);
