6bb68819c5e3886bd0364a9beaf6c81c8b27fb6f
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-24 13:48:41 +0700

    семинар 2 задача 1

---
+++ sem02_01/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int userNumber = Prompt("Введите трехзначное число > ");
+if (userNumber < 1000 && userNumber > 99)
+{
+    int result = userNumber % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
