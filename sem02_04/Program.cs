82f0006ce688ba7c13239600a4a8ed9d4603ab05
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-24 14:37:53 +0700

    семинар 2 задача 4

---
+++ sem02_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.Write("Введите число > ");
+int value = int.Parse(Console.ReadLine() ?? "0");
+bool remains = (value % 7 == 0) && (value % 23 == 0);
+if (remains)
+{
+    Console.WriteLine($"{value} -> да");
+}
+else
+{
+    Console.WriteLine($"{value} -> нет");
+}
