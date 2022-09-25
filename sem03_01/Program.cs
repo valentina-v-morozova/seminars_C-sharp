57621283eb422f7614302d88a0bc62036d337e37
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-25 14:49:40 +0700

    семинар 3 задача 1

---
+++ sem03_01/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool ValidateCoords(int x, int y) //Валидация данных
+{
+    if (!(x == 0 || y == 0)) // (!(x == 0) && !(y == 0)) или (x != 0 && y != 0)
+    {
+        return true;
+    }
+    Console.WriteLine("Точка лежит на одной из осей");
+    return false;
+}
+
+void DeterminateQuarter(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Первая четверть");
+    }
+    if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Вторая четверть");
+    }
+    if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Третья четверть");
+    }
+    if (x > 0 && y < 0)
+    {
+        Console.WriteLine("Четвертая четверть");
+    }
+}
+
+int x = Prompt("Введите x > ");
+int y = Prompt("Введите y > ");
+if (ValidateCoords(x, y))
+{
+    DeterminateQuarter(x, y);
+}
