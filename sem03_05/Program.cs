2223208b9da728f4992cd516f7b3784c5f8cb88e
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-09-25 16:32:02 +0700

    семинар 3 задача 5

---
+++ sem03_05/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая генерирует последовательность случайных чисел из 10 элементов в диапазоне от 1 до 10, и подсчитывает, сколько сгенерировалось чисел больше 5.
+int size = 10;
+
+void Print(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]:f2} \t"); // \t - догонит до 8 символов
+    }
+    Console.WriteLine();
+}
+
+double[] GenerateArray(int size)
+{
+    double[] numbers = new double[size];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 10) + new Random().NextDouble(); //
+    }
+    return numbers;
+}
+int Count(double[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 5)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+double[] numbers = GenerateArray(size);
+
+Print(numbers);
+System.Console.WriteLine(Count(numbers));
