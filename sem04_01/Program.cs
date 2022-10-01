9a88eb47697b8f661e71d1a721dfe7abef98efc2
valentina_trunova <valentina.v.trunova@yandex.ru>
2022-10-01 16:41:09 +0700

    семинар 4 задача 1

---
+++ sem04_01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+ulong Prompt(string message) //беззнаковое длинное
+{
+    Console.Write(message);
+    return Convert.ToUInt64(Console.ReadLine());
+}
+
+ulong SumNumbers(ulong number)
+{
+    ulong sum = 0;
+    for (ulong i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+ulong num = Prompt("Введите число > ");
+ulong result = SumNumbers(num);
+System.Console.WriteLine($"Сумма чисел равна {result}");
